fde19ec946e9633ecb7939db69ec6afa654f26af
JoyceSilva <joycekvs99@gmail.com>
2025-07-01 14:20:04 -0300

    SIM_Monitor + inicio da homepage

---
+++ SIM_Monitor/iotproject/iotmonitor/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.thing_list, name='thing_list'),
+    path('things/<int:pk>/', views.thing_detail, name='thing_detail'),
+    path('sensors/<int:pk>/readings/csv/', views.export_readings_csv, name='export_readings_csv'),
+    path('readings', views.new_reading, name='new_reading'),
+]
---
+++ SIM_Monitor/iotproject/iotmonitor/views.py
@@ -0,0 +1,90 @@
+import csv
+
+from awesome_django_timezones.middleware import TimezonesMiddleware
+from django.contrib.auth import authenticate
+from django.db.models import Count
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.shortcuts import render
+from django.utils import timezone
+from django.utils.decorators import decorator_from_middleware
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+from .models import Thing, TypeOfThing, Sensor
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyBdK5CVEPB_Mtx9kgR3Ah-_3THvCna5Dq0",
+    "authDomain": "hidratest-183cf.firebaseapp.com",
+    "databaseURL": "https://hidratest-183cf.firebaseio.com",
+    "projectId": "hidratest-183cf",
+    "storageBucket": "hidratest-183cf.appspot.com",
+    "messagingSenderId": "117492214036",
+    "appId": "1:117492214036:web:484f1ae8a56b7c8213fa01"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+def homepage(request):
+    return render(request, 'iotmonitor/homepage.html')
+
+def thing_list(request):
+    types = TypeOfThing.objects.all().order_by('name').annotate(things_count=Count('thing'))
+    things_without_type = Thing.objects.filter(type_of_thing__isnull=True)
+    return render(request, 'iotmonitor/thing_list.html',
+                  {'types': types, 'things_without_type': things_without_type})
+
+timezone_decorator = decorator_from_middleware(TimezonesMiddleware)
+
+@timezone_decorator
+def thing_detail(request, pk):
+    thing = Thing.objects.get(pk=pk)
+    sensors = thing.sensor_set.all().order_by('name')
+    return render(request, 'iotmonitor/thing_detail.html',
+                  {'thing': thing, 'sensors': sensors})
+
+
+# use the csrf_exempt tag since this view will be accessed from a external device
+@csrf_exempt
+@require_POST
+def new_reading(request):
+    # use POST parameters to authenticate
+    user = authenticate(username=request.POST.get('sensor'), password=request.POST.get('password'))
+    if user is not None:
+        try:
+            user.sensor.reading_set.create(value=float(request.POST.get('value')))
+            user.sensor.save()
+            return HttpResponse('Sensor authenticated and data succesfully recorded.')
+
+        except:
+            return HttpResponseBadRequest(
+                'Sensor authenticated but unable to record data. Make sure your sensor provided a value that can be parsed onto a numerical value.')
+    else:
+        response = HttpResponseForbidden(
+            'Sensor not authenticated. Please check its username ({}) and password.'.format(request.POST.get('sensor')))
+        return response
+
+def export_readings_csv(request, pk):
+    sensor = Sensor.objects.get(pk=pk)
+    thing_name = sensor.thing_monitored.name
+    readings = sensor.reading_set.all().values_list('created_date', 'value')
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="readings_{}_{}.csv"'.format(thing_name, sensor.name)
+
+    writer = csv.writer(response)
+    writer.writerow(['Thing', 'Sensor', 'Date', 'Time', 'Timezone', 'Value'])
+
+    for reading in readings:
+        reading_local_datetime = timezone.localtime(reading[0])
+        row = [
+            thing_name,
+            sensor.name,
+            reading_local_datetime.date(),
+            reading_local_datetime.strftime('%H:%M:%S'),
+            reading_local_datetime.strftime('%z'),
+            reading[1]]
+        writer.writerow(row)
+
+    return response
+
