eb75a4f83be22a1111c2347b78e3e5000494e6d3
JoyceSilva <joycekvs99@gmail.com>
2025-07-02 15:32:45 -0300

    Atualização das telas de login e cadastro

--- SIM_Monitor/iotproject/iotmonitor/urls.py
+++ SIM_Monitor/iotproject/iotmonitor/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('readings', views.new_reading, name='new_reading'),
     path('login/', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
+    path('thing_list/', views.thing_list, name='thing_list'),
+    
 ]
